d4cfffdeae9cff853da78e15c38d4fae62ef6658
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-04-29 00:32:46 +0300

    add initDb comments

--- backend/initDb.ts
+++ backend/initDb.ts
@@ -1,11 +1,15 @@
 import { readFileSync } from "fs";
 import { pool, initPool } from "./db";
 
+// Initialise local postgres database
 const init = async () => {
+    // Create fotleague database if it doesn't exist
     const db = await initPool.query("SELECT datname FROM pg_catalog.pg_database WHERE datname = 'fotleague'");
     if (db.rowCount === 0) {
         await initPool.query("CREATE DATABASE fotleague;");
     }
+
+    // Run initial database schema query
     const initQuery = readFileSync("./sql/init.sql").toString();
     await pool.query(initQuery);
     console.log("Database initialized!");
