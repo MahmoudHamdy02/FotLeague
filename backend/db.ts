0c9b683157b6071b84d1cd00fdb179839e42e235
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-05-27 02:02:20 +0300

    add test database

--- backend/db.ts
+++ backend/db.ts
@@ -1,7 +1,11 @@
 import { Pool } from "pg";
 
+const host = process.env.NODE_ENV === "test"
+    ? "db_test"
+    : "db";
+
 export const pool = new Pool({
-    host: "db",
+    host: host,
     port: 5432,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
