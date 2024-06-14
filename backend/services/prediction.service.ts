03d32aa5e77563bd9084ef1598e82559d2576a49
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-06-14 08:13:39 +0300

    use only camelCase in ts

--- backend/services/prediction.service.ts
+++ backend/services/prediction.service.ts
@@ -1,9 +1,8 @@
 import { pool } from "../db";
 import { Prediction } from "../types/Prediction";
 
-// TODO: Unify case between backend and db for user_id/userId etc
-export const getUserPredictionsBySeason = async (user_id: number, season: string): Promise<Prediction[]> => {
-    const data = await pool.query<Prediction>("SELECT p.user_id, p.match_id, p.home, p.away FROM predictions AS p JOIN matches AS m ON p.match_id = m.id WHERE m.season=$1 AND p.user_id=$2;", [season, user_id]);
+export const getUserPredictionsBySeason = async (userId: number, season: string): Promise<Prediction[]> => {
+    const data = await pool.query<Prediction>("SELECT p.user_id, p.match_id, p.home, p.away FROM predictions AS p JOIN matches AS m ON p.match_id = m.id WHERE m.season=$1 AND p.user_id=$2;", [season, userId]);
     return data.rows;
 
 };
@@ -18,18 +17,18 @@ export const addPrediction = async (userId: number, matchId: number, home: numbe
     return data.rows[0];
 };
 
-export const editPredictionScores = async (user_id: number, match_id: number, home: number, away: number): Promise<Prediction> => {
+export const editPredictionScores = async (userId: number, matchId: number, home: number, away: number): Promise<Prediction> => {
     const data = await pool.query("UPDATE predictions SET home = $1, away = $2 WHERE user_id = $3 AND match_id = $4 RETURNING *", [
         home,
         away,
-        user_id,
-        match_id
+        userId,
+        matchId
     ]);
     return data.rows[0];
 };
 
-export const getMatchPredictions = async (match_id: number): Promise<Prediction[]> => {
-    const data = await pool.query<Prediction>("SELECT * FROM predictions WHERE match_id = $1", [match_id]);
+export const getMatchPredictions = async (matchId: number): Promise<Prediction[]> => {
+    const data = await pool.query<Prediction>("SELECT * FROM predictions WHERE match_id = $1", [matchId]);
     return data.rows;
 };
 
