5c2a1b2076a92b53697e5010155b309c448965ab
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-07-14 17:57:37 +0300

    add more screen ui

--- android/FotLeague/app/src/main/java/com/example/fotleague/screens/more/MoreScreen.kt
+++ android/FotLeague/app/src/main/java/com/example/fotleague/screens/more/MoreScreen.kt
@@ -1,22 +1,118 @@
 package com.example.fotleague.screens.more
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.fotleague.LocalAuthUser
+import com.example.fotleague.R
+import com.example.fotleague.ui.theme.Background
+import com.example.fotleague.ui.theme.FotLeagueTheme
+import com.example.fotleague.ui.theme.LightGray
 
 @Composable
 fun MoreScreen(
     viewModel: MoreViewModel = hiltViewModel()
 ) {
-    Text(text = "More screen")
 
     val authUser = LocalAuthUser.current
 
-    if (authUser.isLoggedIn) {
-        Button(onClick = { viewModel.onEvent(MoreEvent.Logout) }) {
-            Text(text = "Logout")
+    MoreContent(
+        isLoggedIn = authUser.isLoggedIn,
+        onLogOutClick = { viewModel.onEvent(MoreEvent.Logout) }
+    )
+}
+
+@Composable
+private fun MoreContent(
+    isLoggedIn: Boolean,
+    onLogOutClick: () -> Unit
+) {
+    Scaffold(
+        topBar = { TopBar() },
+        contentWindowInsets = WindowInsets(0.dp)
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .background(Background)
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            RowButton(
+                icon = ImageVector.vectorResource(id = R.drawable.bug_report_24),
+                text = "Feedback and bug reports",
+                onClick = {}
+            )
+            if (isLoggedIn) {
+                RowButton(
+                    icon = ImageVector.vectorResource(id = R.drawable.logout_24),
+                    text = "Log out",
+                    onClick = onLogOutClick
+                )
+            }
         }
+    }
+}
+
+@Composable
+private fun RowButton(icon: ImageVector, text: String, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .clickable { onClick() }
+            .padding(horizontal = 8.dp),
+
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Icon(imageVector = icon, contentDescription = null, tint = LightGray)
+        Text(text = text)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar() {
+    TopAppBar(
+        title = { Text(text = "More") },
+        windowInsets = WindowInsets(0.dp),
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Background)
+    )
+}
+
+@Preview
+@Composable
+private fun MoreContentPreview() {
+    FotLeagueTheme {
+        MoreContent(
+            isLoggedIn = true,
+            onLogOutClick = {}
+        )
     }
 }
