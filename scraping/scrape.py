713d12883beaac958044bddabc7c835f0d9bfd77
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-12-31 02:54:36 +0200

    update scraping logic

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -5,9 +5,14 @@ import requests
 
 # FotMob website
 url = "https://www.fotmob.com/api/leagues?id=47"
-backend = "http://localhost:3001"
+backend = "http://backend:3000"
+
 # Wait until backend starts up
-# sleep(30)
+sleep(15)
+
+# Get token from fotmob website
+x_fm_req = requests.get('http://46.101.91.154:6006/')
+token = x_fm_req.json()
 
 # Health check
 print("Pinging backend")
@@ -22,15 +27,14 @@ while True:
 
     matches = requests.get(f"{backend}/matches/{currentSeason}").json()
 
-    data = {}
-    with open('data.json') as f:
-        data = json.load(f)
+    fotmob = requests.get(url, headers=token)
+    data = json.loads(fotmob.text)
 
     updatedMatches = data["matches"]["allMatches"]
 
     # Get corresponding updated matches for each database match
     for match in matches:
-        updatedMatchList = list(filter(lambda updatedMatch: match['gameweek'] == int(updatedMatch["round"]) and match['home'] == updatedMatch["home"]["name"] and match['away'] == updatedMatch["away"]["name"], updatedMatches))
+        updatedMatchList = list(filter(lambda updatedMatch: match['gameweek'] == int(updatedMatch["round"]) and match['home'] == updatedMatch["home"]["shortName"] and match['away'] == updatedMatch["away"]["shortName"], updatedMatches))
 
         # len == 1: Match hasn't been aborted
         if len(updatedMatchList) == 1:
@@ -38,7 +42,8 @@ while True:
         # len > 1: Match has been aborted, only use latest entry
         else:
             res = sorted(updatedMatchList, reverse=True, key=lambda x: datetime.strptime(x["status"]["utcTime"], fotmob_date_format))
-        
+            updatedMatch = res[0]
+
         if updatedMatch["status"]["cancelled"]:
             status = 4
         elif updatedMatch["status"]["finished"]:
@@ -49,7 +54,7 @@ while True:
             status = 1
 
         # Get game score if it has started
-        if updatedMatch["status"]["scoreStr"]:
+        if status == 2 or status == 3 and updatedMatch["status"]["scoreStr"]:
             home_score = int(updatedMatch["status"]["scoreStr"].split("-")[0].strip())
             away_score = int(updatedMatch["status"]["scoreStr"].split("-")[1].strip())
         else:
