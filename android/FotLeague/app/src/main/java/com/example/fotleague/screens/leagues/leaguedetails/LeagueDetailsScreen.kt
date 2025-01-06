23933d5c80d89e04d25aaea73f96e01fce06eb4f
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-01-07 01:49:29 +0200

    refactor dialogs to separate files

--- android/FotLeague/app/src/main/java/com/example/fotleague/screens/leagues/leaguedetails/LeagueDetailsScreen.kt
+++ android/FotLeague/app/src/main/java/com/example/fotleague/screens/leagues/leaguedetails/LeagueDetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.fotleague.screens.leagues.leaguedetails
 
-import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -14,12 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -41,6 +38,8 @@ import com.example.fotleague.LocalTopBar
 import com.example.fotleague.R
 import com.example.fotleague.Screen
 import com.example.fotleague.models.UserScore
+import com.example.fotleague.screens.leagues.components.ConfirmDeleteLeagueDialog
+import com.example.fotleague.screens.leagues.components.ConfirmLeaveLeagueDialog
 import com.example.fotleague.ui.components.ScoresTable
 import com.example.fotleague.ui.navigation.AppBarState
 import com.example.fotleague.ui.theme.Background
@@ -184,72 +183,6 @@ private fun LeagueDetailsContent(
         ConfirmDeleteLeagueDialog(
             onDeleteLeague = onDeleteLeague,
             onDismiss = onDismissDeleteLeagueDialog
-        )
-    }
-}
-
-@Composable
-private fun ConfirmLeaveLeagueDialog(
-    onDismiss: () -> Unit,
-    onLeaveLeague: () -> Unit
-) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text(text = "Leave league") },
-        text = { Text(text = "Are you sure you want to leave this league?") },
-        confirmButton = {
-            TextButton(onClick = onLeaveLeague) {
-                Text(text = "Leave")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = "Cancel")
-            }
-        }
-    )
-}
-
-@Composable
-private fun ConfirmDeleteLeagueDialog(
-    onDismiss: () -> Unit,
-    onDeleteLeague: () -> Unit
-) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text(text = "Delete league") },
-        text = { Text(text = "Are you sure you want to delete this league?") },
-        confirmButton = {
-            TextButton(onClick = onDeleteLeague) {
-                Text(text = "Delete")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = "Cancel")
-            }
-        }
-    )
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun ConfirmLeaveLeagueDialogPreview() {
-    FotLeagueTheme {
-        ConfirmLeaveLeagueDialog(
-            onLeaveLeague = {},
-            onDismiss = {}
-        )
-    }
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun ConfirmDeleteLeagueDialogPreview() {
-    FotLeagueTheme {
-        ConfirmDeleteLeagueDialog(
-            onDeleteLeague = {},
-            onDismiss = {}
         )
     }
 }
