e005a30e98f8c427ed393ffd48cb1f6673dcd346
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-01-07 23:08:02 +0200

    cleanup

--- android/FotLeague/app/src/main/java/com/example/fotleague/MainViewModel.kt
+++ android/FotLeague/app/src/main/java/com/example/fotleague/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.fotleague
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fotleague.data.FotLeagueApi
@@ -12,17 +11,12 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(val api: FotLeagueApi, val authStatus: AuthStatus) :
     ViewModel() {
 
-//    private val _state = MutableStateFlow(MainState())
-//    val state: StateFlow<MainState> = _state.asStateFlow()
-
     fun init() {
-        Log.d("AUTH", "{userResponse.code()}")
         viewModelScope.launch {
             try {
                 val userResponse = api.getAuthUser()
                 val body = userResponse.body()
                 if (userResponse.code() == 200 && body != null) {
-//                    _state.update { state -> state.copy(isLoggedIn = true, isLoading = false, user = body) }
                     authStatus.setAuthState(
                         AuthState(
                             user = userResponse.body(),
@@ -31,20 +25,11 @@ class MainViewModel @Inject constructor(val api: FotLeagueApi, val authStatus: A
                         )
                     )
                 } else {
-//                    _state.update { state -> state.copy(isLoggedIn = false, isLoading = false) }
                     authStatus.setAuthState(AuthState(isLoading = false, isLoggedIn = false))
                 }
             } catch (e: Exception) {
-//                _state.update { it.copy(error = "Connection failed") }
                 authStatus.setAuthState(AuthState(error = "Connection failed", isLoading = false))
             }
         }
     }
 }
-
-//data class MainState(
-//    val error: String? = null,
-//    val isLoggedIn: Boolean = false,
-//    val isLoading: Boolean = true,
-//    val user: User = User(0, "", "", 2)
-//)
--- android/FotLeague/app/src/main/java/com/example/fotleague/data/AddCookiesInterceptor.kt
+++ android/FotLeague/app/src/main/java/com/example/fotleague/data/AddCookiesInterceptor.kt
@@ -1,8 +1,5 @@
 package com.example.fotleague.data
 
-import android.util.Log
-import kotlinx.coroutines.flow.last
-import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -14,10 +11,7 @@ class AddCookiesInterceptor(private val dataStoreUtil: DataStoreUtil) : Intercep
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder: Request.Builder = chain.request().newBuilder()
 
-        Log.d("TEST", "intercept: 1")
         val cookie = dataStoreUtil.getAuthCookie.value
-        Log.d("INTERCEPTOR COOKIE", cookie)
-
         builder.addHeader("Cookie", cookie)
 
         return chain.proceed(builder.build())
