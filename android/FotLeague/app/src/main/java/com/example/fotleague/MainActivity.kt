74da021db8b2d37aac67c5147cc004da2132be2f
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-01-09 03:07:00 +0200

    cleanup

--- android/FotLeague/app/src/main/java/com/example/fotleague/MainActivity.kt
+++ android/FotLeague/app/src/main/java/com/example/fotleague/MainActivity.kt
@@ -15,13 +15,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.fotleague.ui.navigation.AppBarState
 import com.example.fotleague.ui.navigation.BottomNavigation
 import com.example.fotleague.ui.theme.FotLeagueTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,10 +37,6 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry?.destination?.route
-
-                var appBarState by remember {
-                    mutableStateOf(AppBarState())
-                }
 
                 Scaffold(
                     contentWindowInsets = ScaffoldDefaults.contentWindowInsets.exclude(WindowInsets.statusBars),
