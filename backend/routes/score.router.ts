58b9e5a54aceb02f568e5ae27559f92a2f0d1256
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-03-19 09:01:26 +0200

    validate score routes

--- backend/routes/score.router.ts
+++ backend/routes/score.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/requireAuth";
 import { scoreController } from "../controllers/score.controller";
+import { validator } from "../validators/validator";
+import { validate } from "../validators/validate";
 
 export const scoreRouter = Router();
 
@@ -8,9 +10,9 @@ scoreRouter.get("/user", requireAuth, scoreController.getUserScores);
 
 scoreRouter.get("/user/gameweeks", requireAuth, scoreController.getUserGameweekScores);
 
-scoreRouter.get("/user/:season", requireAuth, scoreController.getUserScoresBySeason);
+scoreRouter.get("/user/:season", requireAuth, validator.season, validate, scoreController.getUserScoresBySeason);
 
-scoreRouter.get("/user/total/:season", requireAuth, scoreController.getTotalUserScoreBySeason);
+scoreRouter.get("/user/total/:season", requireAuth, validator.season, validate, scoreController.getTotalUserScoreBySeason);
 
 // TODO: Needs tests
 scoreRouter.get("/global", scoreController.getTopGlobalUsers);
